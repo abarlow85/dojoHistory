5a11d7c5d3a7d4bb518b3882b4ac74e1cbe44f32
Gabe Ratcliff <gaberatcliff@Zoes-MacBook-Air-3.local>
2016-05-12 19:31:29 -0700

    added current location functionality

--- dojoHistory/ViewController.swift
+++ dojoHistory/ViewController.swift
@@ -11,11 +11,13 @@ import MapKit
 import CoreLocation
 import AddressBookUI
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
  
     @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var pointsOfInterst = [[String:Double]]()
+
     
-    var locations = [[String:Double]]()
     //    var didLoad = false
     //FORWARD GEOCODING
     //    func forwardGeocoding(address: String){
@@ -34,43 +36,25 @@ class ViewController: UIViewController, MKMapViewDelegate {
     //                return self.viewDidLoad()
     //            }
     //        })
-    ////        print(locations)
+    ////        print(pointsOfInterst)
     //    }
-    func didReceiveCoords(location: NSDictionary){
-        locations.append(location as! [String : Double])
-        print(locations)
-    }
-    
-    //center on page load
-    func centerMapOnLocation(location: MKPointAnnotation, regionRadius: Double){
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
-        mapView.setRegion(coordinateRegion, animated: true)
-    }
-    //information "button"
-    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-        let reuseIdentifier = "pin"
-        var pin = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseIdentifier) as? MKPinAnnotationView
-        if pin == nil {
-            pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseIdentifier)
-            pin!.pinColor = .Red
-            pin!.canShowCallout = true
-            pin!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
-        } else {
-            pin!.annotation = annotation
-        }
-        return pin
-    }
-
     
     override func viewDidLoad() {
-        let model = LocationModel()
-        let locations = model.data
-        print(locations)
-        mapView.delegate = self
         super.viewDidLoad()
+        
+        let model = LocationModel()
+        let pointsOfInterst = model.data
+        mapView.delegate = self
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+        
         //build annotation pins
         var annotations = [MKPointAnnotation]()
-        for dictionary in locations {
+        for dictionary in pointsOfInterst {
             let latitude = CLLocationDegrees(dictionary["Latitude"] as! Double!)
             let longitude = CLLocationDegrees(dictionary["Longitude"] as! Double!)
             let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
@@ -80,11 +64,43 @@ class ViewController: UIViewController, MKMapViewDelegate {
             annotation.title = "\(name)"
             annotations.append(annotation)
         }
-//        print(annotations)
+        //        print(annotations)
         mapView.addAnnotations(annotations)
-        centerMapOnLocation(annotations[0], regionRadius: 50000.0)
         
     }
+
+    func didReceiveCoords(location: NSDictionary){
+        pointsOfInterst.append(location as! [String : Double])
+        print(pointsOfInterst)
+    }
     
+//    //information "button"
+//    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+//        let reuseIdentifier = "pin"
+//        var pin = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseIdentifier) as? MKPinAnnotationView
+//        if pin == nil {
+//            pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseIdentifier)
+//            pin!.pinColor = .Red
+//            pin!.canShowCallout = true
+//            pin!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+//        } else {
+//            pin!.annotation = annotation
+//        }
+//        return pin
+//    }
+    //Find users locations
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        self.mapView.setRegion(region, animated: true)
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Error: " + error.localizedDescription)
+    }
+    
+
 }
 
